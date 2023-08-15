e35780255a5ff837f73706f52a3336070977e0ab
Kroggieboy57 <menonkrishna57@gmail.com>
2023-08-15 20:05:23 +0530

    Updated the code according to the question

--- 4-08-23/q2.cpp
+++ 4-08-23/q2.cpp
@@ -2,13 +2,26 @@
 using namespace std;
 int main()
 {
-    char name[100];
+    char name[100],city[100],gen;
     int age;
+    float height;
     cout<<"Enter name: ";
     cin.getline(name,100);
     cout<<"Enter Age: ";
     cin>>age;
+    cout<<"Enter Gender(M/F): ";
+    cin>>gen;
+    cout<<"Enter City: ";
+    cin>>city;
+    cout<<"Enter Height: ";
+    cin>>height;
     cout<<"Name: "<<name<<endl;
     cout<<"Age: "<<age<<endl;
+    cout<<"Gender: "<<gen<<endl;
+    cout<<"City: "<<city<<endl;
+    cout<<"Height: "<<height<<endl;
     return 0;
 }
+
+    
+    
