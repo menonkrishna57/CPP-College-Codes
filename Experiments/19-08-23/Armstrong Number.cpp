37cccd1d86674093ea3601fe7d63a23cd937e41d
Kroggieboy57 <menonkrishna57@gmail.com>
2023-08-27 19:02:39 +0530

    Added some more codes I did in college. Havent sorted them yet

---
+++ Experiments/19-08-23/Armstrong Number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>//for cout and cin
+#include <math.h>//for pow() function
+using namespace std;//for cout and cin
+int main()
+{
+    int armstong=0,n=0,m=0,c=0;//initializing variables
+    cout<<"Enter Number: ";//taking input
+    cin>>n;
+    m=n;//making another copy of n so that the original value stays safe after all the operations i do on it in the do while loop
+    do
+    {
+        n/=10;//This removes the last digit of n and the second last digit becomes the last
+        c++;//This counts the number of digits in the number
+    }while(n!=0);
+    n=m;//making another copy of n so that the original value stays safe after all the operations i do on it in the do while loop
+    do
+    {
+       armstong+=pow(m%10,c);//Taking the power of the digits and adding them to armstong
+        m/=10;//This removes the last digit of m and the second last digit becomes the last
+    }while(m!=0);
+    if (armstong==n)
+    {
+        cout<<"It is an armstrong number";//If the sum of the powers of the digits is equal to the original number then it is an armstrong number
+    }
+    else{
+        cout<<armstong<<"Not an armstrong number";//If the sum of the powers of the digits is not equal to the original number then it is not an armstrong number
+    }
+}
