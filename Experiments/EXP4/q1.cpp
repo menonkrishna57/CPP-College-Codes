18439921bf6df2ff8e1683bc07cd908ddc0b8325
Kroggieboy57 <menonkrishna57@gmail.com>
2023-08-28 12:44:08 +0530

    Added a few comments

--- Experiments/EXP4/q1.cpp
+++ Experiments/EXP4/q1.cpp
@@ -4,13 +4,13 @@ int main()
 {
     int n=0,mysum=0;
     int i=1;
-    cout<<"Enter Number:";
+    cout<<"Enter Number:";//taking input
     cin>>n;
     while(i<=n)
     {
-        mysum+=2*i;
+        mysum+=2*i;//adding 2*i to mysum
         i++;
     }
-    cout<<"Sum is: "<<mysum;
+    cout<<"Sum is: "<<mysum;//printing mysum
     return 0;
 }
