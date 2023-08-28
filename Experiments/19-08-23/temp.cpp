a4ef9929ec2952b78d25628e162356fd59f753fa
Kroggieboy57 <menonkrishna57@gmail.com>
2023-08-28 14:49:53 +0530

    Temp file

---
+++ Experiments/19-08-23/temp.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int num1,num,rev;
+    // cout<<"Enter Number: ";
+    // cin>>num;
+    // num1=num;
+    // rev=0;
+    // while(num!=0)
+    // {
+    //     rev=(rev*10)+num%10;
+    //     num=num/10;
+    // }
+    // if(num1==rev)
+    // {
+    //     cout<<"It is a Palindrome";
+    // }
+    // else
+    // {
+    //     cout<<"Not a Palindrome";
+    int a=1;
+    int b=a==1?0:1;
+}
