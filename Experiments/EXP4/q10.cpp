c8aa4b9dd49c110c20cc8f56881e54f99582774d
Kroggieboy57 <menonkrishna57@gmail.com>
2023-08-28 08:40:21 +0530

    Prints leap years from 1 to N

---
+++ Experiments/EXP4/q10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter Number: ";
+    cin>>n;
+    cout<<"Printing leap years:"<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%4==0&&i%100!=0||i%400==0)
+        {
+            cout<<i<<endl;
+        }
+    }
+}
