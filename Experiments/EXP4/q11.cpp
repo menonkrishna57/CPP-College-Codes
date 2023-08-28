a4cfbc71d9694effb5a7e9897cee3777918ccec1
Kroggieboy57 <menonkrishna57@gmail.com>
2023-08-28 08:40:44 +0530

    Working Prime Number Checker

---
+++ Experiments/EXP4/q11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    string res="Prime Number";
+    cout<<"Enter Number: ";
+    cin>>n;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            res="Not a prime Number";
+            break;
+        }
+    }
+    cout<<res;
+}
