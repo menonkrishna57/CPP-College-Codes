2a29942ec02569c88fa971b3164428d3bba13f4a
Kroggieboy57 <menonkrishna57@gmail.com>
2023-08-28 14:49:41 +0530

    Comments colour changed

--- Experiments/19-08-23/Palindrome.cpp
+++ Experiments/19-08-23/Palindrome.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 int main()
 {
-    int palin=0,n=0,m=0,c=0; //Initializing all variables
+    int palin=0,n=0,m=0,c=0; //!Initializing all variables
     cout<<"Enter Number: ";
     cin>>n;
-    m=n;//Making another copy of n so that the original value stays safe after all the operations i do on it in the do while loop
-    do//This loop reverses the number
+    m=n;//!Making another copy of n so that the original value stays safe after all the operations i do on it in the do while loop
+    do//!This loop reverses the number
     {
-        palin+=(n%10);//This gives the last digit of n
-        palin*=10;//The last digit i got is multiplied by 10 so that i can when i can add the second last digit to the this
-        n/=10;//This removes the last digit of n and the second last digit becomes the last
+        palin+=(n%10);//!This gives the last digit of n
+        palin*=10;//!The last digit i got is multiplied by 10 so that i can when i can add the second last digit to the this
+        n/=10;//!This removes the last digit of n and the second last digit becomes the last
 
 
     }while(n!=0);
-    palin/=10;//This removes the last digit of palin which is 0
+    palin/=10;//!This removes the last digit of palin which is 0
     if (palin==m)
     {
-        cout<<"It is a Palindrome";//If the reversed number is equal to the original number then it is a palindrome
+        cout<<"It is a Palindrome";//!If the reversed number is equal to the original number then it is a palindrome
     }
     else{
-        cout<<"Not a Palindrome";//If the reversed number is not equal to the original number then it is not a palindrome
+        cout<<"Not a Palindrome";//!If the reversed number is not equal to the original number then it is not a palindrome
             }
 }
