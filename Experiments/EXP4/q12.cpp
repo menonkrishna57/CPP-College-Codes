904d707c8974cad39f02ea1b111cd62f567cd65e
Kroggieboy57 <menonkrishna57@gmail.com>
2023-09-06 20:33:30 +0530

    Reformated it

--- Experiments/EXP4/q12.cpp
+++ Experiments/EXP4/q12.cpp
@@ -1,17 +1,19 @@
-//Write a program to print the entire uppercase and lowercase letters using a loop (use continue statement).
+//! Write a program to print the entire uppercase and lowercase letters using a loop (use continue statement).
 #include <iostream>
 using namespace std;
 int main()
 {
     char i;
-    for (i='A';i>='A' && i<='z';i++)
+    for (i = 'A'; i >= 'A' && i <= 'z'; i++)
     {
-        
-        if(i>'Z' && i<'a')
+
+        if (i > 'Z' && i < 'a')
         {
             continue;
         }
-        else{
-        cout<<i<<endl;}
+        else
+        {
+            cout << i << endl;
+        }
     }
 }
