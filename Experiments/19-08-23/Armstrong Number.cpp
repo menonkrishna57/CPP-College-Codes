813636d47e77ea7186fcabf00230c6b44d32c4c8
“Krishna <menonkrishna57@gmail.com>
2023-09-11 23:32:49 +0530

    Variable Spelling Corrected

--- Experiments/19-08-23/Armstrong Number.cpp
+++ Experiments/19-08-23/Armstrong Number.cpp
@@ -3,7 +3,7 @@
 using namespace std;//!for cout and cin
 int main()
 {
-    int armstong=0,n=0,m=0,c=0;//!initializing variables
+    int armstrong=0,n=0,m=0,c=0;//!initializing variables
     cout<<"Enter Number: ";//!taking input
     cin>>n;
     m=n;//!making another copy of n so that the original value stays safe after all the operations i do on it in the do while loop
@@ -15,14 +15,14 @@ int main()
     n=m;//!making another copy of n so that the original value stays safe after all the operations i do on it in the do while loop
     do
     {
-       armstong+=pow(m%10,c);//!Taking the power of the digits and adding them to armstong
+       armstrong+=pow(m%10,c);//!Taking the power of the digits and adding them to armstrong
         m/=10;//!This removes the last digit of m and the second last digit becomes the last
     }while(m!=0);
-    if (armstong==n)
+    if (armstrong==n)
     {
         cout<<"It is an armstrong number";//!If the sum of the powers of the digits is equal to the original number then it is an armstrong number
     }
     else{
-        cout<<armstong<<"Not an armstrong number";//!If the sum of the powers of the digits is not equal to the original number then it is not an armstrong number
+        cout<<armstrong<<"Not an armstrong number";//!If the sum of the powers of the digits is not equal to the original number then it is not an armstrong number
     }
 }
