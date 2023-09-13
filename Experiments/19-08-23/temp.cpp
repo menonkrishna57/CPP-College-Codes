903163ae9a9a4a4e5bf4d3a382695b0ad74dfd6e
menonkrishna57 <menonkrishna57@gmail.com>
2023-09-13 23:15:02 +0530

    Temp file code changed for testing

--- Experiments/19-08-23/temp.cpp
+++ Experiments/19-08-23/temp.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 int main()
 {
-    // int num1,num,rev;
-    // cout<<"Enter Number: ";
-    // cin>>num;
-    // num1=num;
-    // rev=0;
-    // while(num!=0)
-    // {
-    //     rev=(rev*10)+num%10;
-    //     num=num/10;
-    // }
-    // if(num1==rev)
-    // {
-    //     cout<<"It is a Palindrome";
-    // }
-    // else
-    // {
-    //     cout<<"Not a Palindrome";
-    int a=1;
-    int b=a==1?0:1;
+    int n = 3;  // Number of rows in the pattern
+
+    for (int i = 0; i < n; i++) {
+        
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << "|";
+        }
+
+        
+        for (int j = 0; j <= i; j++) {
+            cout << "$";
+            if (j < i) {
+                cout << "|";
+            }
+        }
+
+        cout << endl;
+    }
+    return 0;
 }
