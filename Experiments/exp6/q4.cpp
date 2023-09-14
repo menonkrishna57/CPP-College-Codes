354dc7aea6e8619eb40e60c25e9cb8b69d9296fa
menonkrishna57 <menonkrishna57@gmail.com>
2023-09-14 13:48:33 +0530

    Printing both the arrays

--- Experiments/exp6/q4.cpp
+++ Experiments/exp6/q4.cpp
@@ -8,15 +8,22 @@ int main()
     int arr[c],rev[c];
     for(int i=0;i<c;i++)
     {
-        cout<<"Enter the value:";
+        cout<<"Enter the number:";
         cin>>arr[i];
     }
     for(int i=c-1;i>=0;i--)
     {
         rev[r++]=arr[i];
     }
+    
+    cout<<"Orignal Array:"<<endl;
     for(int i=0;i<c;i++)
     {
-        cout<<rev[i]<<",";
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\nReversed Array:"<<endl;
+    for(int i=0;i<c;i++)
+    {
+        cout<<rev[i]<<" ";
     }
 }
